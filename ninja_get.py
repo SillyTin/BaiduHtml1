1e314c666a0c45ec93fb4e747177af7c50601b04
SillyTin <Wdding@icloud.com>
2019-04-14 13:38:18 +0800

    dfs

--- ninja_get.py
+++ ninja_get.py
@@ -1,65 +1,95 @@
 from binaryninja import *
 import os
 
-def findpath_cg(start, cg, path, pre = None):
+def findpath_cg(start, end, cg, cg_path):
 
-	if start not in path.keys():
-		path[start] = []
-	
-	if pre:
-		for repath in path[pre]:
-			flag = True
-			for edge in path[start]:
-				if repath == edge[:-1]:
-					flag = False
-			if flag:
-				path[start].append(repath.copy())
-			for edge in path[start]:
-				if edge[-1] != start:
-					edge.append(start)
-	else:
-		path[start].append([start])
-
-	list = cg[start]
-	for item in list:
-		findpath_cg(item, cg, path, start)
+	related_node = [end]
+	find_related_node(end, related_node, cg)
+	path = []
+	dfs(start, end, path, cg_path, cg, related_node)
+	if start == '0x10338':
+		print('11')
 
 
-def findpath_cfg(start, end, cfg_sin, cfg_path_sin, pre = None):
-
+def findpath_cfg(start, end, cfg_sin, cfg_path_sin):
 	# cfg_sin: intro procedure, is a dirc
-	# cfg_path_sin:{node:[[path1],[path2]]}, intro procedure
+	# cfg_path_sin:{node->node:[[path1],[path2]]}, intro procedure
 
-	#traverse the basic blocks in the start_func to find a path to the end_func
+	dst_bb = find_dst_bb(start, end)
+
+	for bb in dst_bb:
+		dominators = []
+		get_dominators(bb, dominators)
+		print('dominators')
+		for i in range(len(dominators)-1):
+			start = hex(int(dominators[i+1].start))
+			end = hex(int(dominators[i].start))
+			name = "%s->%s" % (start,end)
+			print(name)
+			cfg_path_sin[name] = []
+			findpath_cg(start, end, cfg_sin, cfg_path_sin[name])
+			print(cfg_path_sin[name])
+			i += 1
 
 
-	if start not in cfg_path_sin.keys():
-		cfg_path_sin[start] = []
+def find_related_node(end, related_node, graph):
 	
-	if pre:
-		for repath in cfg_path_sin[pre]:
-			flag = True
-			for edge in cfg_path_sin[start]:
-				if repath == edge[:-1]:
-					flag = False
-			if flag:
-				cfg_path_sin[start].append(repath.copy())
-			for edge in cfg_path_sin[start]:
-				if edge[-1] != start:
-					edge.append(start)
+	for i in graph.keys():
+		if end in graph[i]:
+			if i not in related_node:
+				related_node.append(i)
+				find_related_node(i, related_node, graph)
+
+
+def get_dominators(bb, dominators):
+	# bb is binaryninja basic_block object
+	# dominators is a list including dominators of bb
+
+	global bv
+
+	if bb.immediate_dominator:
+		bb = bb.immediate_dominator
+		dominators.append(bb)
+		get_dominators(bb, dominators)
+
+
+def dfs(start, end, path, all_path, graph, related_node):
+	# depth-first-search
+	# start and end are strings of hex address in a graph
+	# path is a list of node in a tmp path
+	# all_path is a list including all paths from start to end
+	# related_node is a list including all nodes related to end
+
+	if start == '0x10338':
+		print('64')
+
+	if start == end:
+		path.append(end)
+		all_path.append(path.copy())
+		path.pop()
 	else:
-		cfg_path_sin[start].append([start])
+		path.append(start)
+		for i in graph[start]:
+			if i not in path and i in related_node:
+				dfs(i, end, path, all_path, graph, related_node)
 
-	list = cfg_sin[start]
-	for item in list:
-		findpath_cfg(item, end, cfg_sin, cfg_path_sin, start)
-		print("findpath_cfg_2\n")
+		path.pop()
 
-	print(cfg_path_sin)
 
-	# for bb in start_func:
-		# find the way
+def find_dst_bb(src, dst):
+	# src and dst are strings of hex address such as find_dst_bb('0x100a0', '0x19b3c')
+	# dst_bb is a list including the bb will call the dst func
 
+	src_addr = int(src,16)
+	function = bv.get_function_at(src_addr)
+
+	dst_bb = []
+	for inst in function.instructions:
+		if str(inst[0][-1]) == dst:
+			dst_addr = int(str(inst[1]))
+			dst_bb.append(function.get_basic_block_at(dst_addr))
+
+	return dst_bb
 
 
 if __name__ == '__main__':
@@ -79,29 +109,32 @@ if __name__ == '__main__':
 		print(path+' already exists')
 
 	# Get functions
-	f = open("%sfunc.txt" % (path) , "w")
 	funcs = {}
 	for func in bv.functions:
 		addr = str(hex(int(func.start)))
 		funcs[addr] = func.symbol.name
+
+	f = open("%sfunc.txt" % (path) , "w")
 	for addr in funcs.keys():
 		f.write("%s : %s\n" % (addr , funcs[addr]))
 	f.close()
+	print("get func")
 
 	# Get instructions
-	f = open("%sinst.txt" % (path) , "w")
 	inst = []
 	for i in bv.instructions:
 		ins = ''
 		for j in i[0]:
 			ins = ins + str(j)
 		inst.append("%s : %s" % (str(hex(int(i[1]))), ins))
+
+	f = open("%sinst.txt" % (path) , "w")
 	for i in inst:
 		f.write("%s\n" % (i))
 	f.close()
+	print("get inst")
 
 	# Create CG
-	f = open("%scg.txt" % (path) , "w")
 	cg = {}
 	for func in bv.functions:
 		caller = str(hex(int(func.start)))
@@ -114,14 +147,15 @@ if __name__ == '__main__':
 			# if str(inst[0][0] == 'b'):
 			# 	des = str(inst[0][-1])
 
+	f = open("%scg.txt" % (path) , "w")
 	for caller in cg.keys():
 		if cg[caller] != []:
 			for callee in cg[caller]:
 				f.write("%s -> %s\n" % (caller , callee))
 	f.close()
+	print("get cg")
 
 	# Create CFG
-	f = open("%scfg.txt" % (path) , "w")
 	cfg = {}
 	for func in bv.functions:
 		func_name = str(hex(int(func.start)))
@@ -133,6 +167,8 @@ if __name__ == '__main__':
 				callee = str(hex(int(edge.target.start)))
 				if callee not in cfg[func_name][caller]:
 					cfg[func_name][caller].append(callee)
+
+	f = open("%scfg.txt" % (path) , "w")
 	for func in cfg.keys():
 		flag = False
 		for item in cfg[func]:
@@ -145,29 +181,28 @@ if __name__ == '__main__':
 					for callee in cfg[func][caller]:
 						f.write("%s -> %s\n" % (caller , callee))
 	f.close()
+	print("get cfg")
 
 	# Find path in func level
-	f = open("%scg_path.txt" % (path) , "w")
-	edge = {}
+	cg_path = []
 	start = '0x100a0'
 	end = '0xd69c'
-	findpath_cg(start, cg, edge)
-	for i in edge[end]:
+	findpath_cg(start, end, cg, cg_path)
+
+	f = open("%scg_path.txt" % (path) , "w")
+	for i in cg_path:
 		for j in i[:-1]:
 			f.write("%s -> " % (j))
 		f.write("%s\n" % (i[-1]))
 	f.close()
+	print("get cg_path")
 
-	#Find path in bb level
-	f = open("%scfg_path.txt" % (path) , "w")
+	# Find path intro func in bb level
 	cfg_path = {}
-	func_path = edge[end]
-	for item in func_path:
-		start = item[0]
-		cfg_path[start] = {}
-		end = item[1]
-		item = item[1:]
-		findpath_cfg(start, end, cfg[start], cfg_path[start])
-		print("findpath_cfg_1\n")
-		
-	f.close()
+	for i in cg_path:
+		for j in range(len(i)-1):
+			start = i[j]
+			end = i[j+1]
+			cfg_path[start] = {}
+			findpath_cfg(start, end, cfg[start], cfg_path[start])
+	print(cfg_path)
